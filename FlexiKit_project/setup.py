098588b1c839b650b6c92564669cbd1559ecfd21
Zach <100164907@ccisd.net>
2025-10-22 19:10:09 -0500

    Update setup.py

--- FlexiKit_project/setup.py
+++ FlexiKit_project/setup.py
@@ -9,12 +9,16 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/your-repo",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={
+        "FlexiKit": ["*.dll"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # or your license
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.0+',
+    python_requires='>=3.0',
 )
-
